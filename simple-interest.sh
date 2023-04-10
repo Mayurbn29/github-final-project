7ba68b235fcfa6554dec7b7f5b6936452bd96bc9
Mayur BN <91716459+Mayurbn29@users.noreply.github.com>
2023-04-10 20:55:46 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Mayurbn29
+   # Mayurbn29 (updated)
    # Input:
    # p, principal amount
    # t, time period in years
